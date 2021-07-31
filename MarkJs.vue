1f73bf4dda799acdeb8e9dfa1544d634d35dea50
Akbar Idria <akbaridria15@gmail.com>
2021-07-31 21:19:05 +0700

    Initial commit

---
+++ MarkJs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="mark">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Mark from 'mark.js';
+
+export default {
+  name: 'mark-js',
+
+  props: {
+    mark: String
+  },
+
+  mounted() {
+    this.instance = new Mark(this.$refs.mark);
+    this.instance.mark(this.mark);
+  },
+
+  watch: {
+    mark(value) {
+      this.instance.unmark();
+      this.instance.mark(value);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
